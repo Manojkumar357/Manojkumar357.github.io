a731075e80aa70a2a904699fb611569f999f0d1b
Manojkumar357 <akulamanojkumar357@gmail.com>
2025-06-24 11:29:03 +0530

    update

--- JavaScript/shoppingcart/app.js
+++ JavaScript/shoppingcart/app.js
@@ -2,23 +2,37 @@
 
 let cart={}
 const products=[
-    {id:1,name:"Product1",price:25},
-    {id:2,name:"Product2",price:100},
-    {id:3,name:"Product3",price:50},
+    {id:1,name:"Apples",price:25},
+    {id:2,name:"Bananas",price:100},
+    {id:3,name:"Oranges",price:50},
 ];
 
 const showProducts=()=>{
     let str=""
     products.map((value)=>{
-         str+=`${value.id}-${value.name}-${value.price}<br>`
+         str+=`<div style="color:blue;">${value.id}-${value.name}-Rs.${value.price}</div>
+         <button class="but">Add to cart</button><br>`
     })
    return str;
 }
 
 function greet(){
-    document.getElementById("root").innerHTML=showProducts()
+    document.getElementById("r").innerHTML=showProducts()
+
+}
+function greet1(){
+        document.getElementById("s").innerHTML=addToCart()
 }
 
-const addToCart=(id)=>{
-    cart={...cart,[id]:1}
+const addToCart=()=>{
+    
+    products.map(value=>{
+        cart[value.id]={...value};
+    })
+    let  str="<h3>Your Cart: </h3>"
+    for(let key in cart){
+        const item=cart[key];
+        str+=`${item.name}-Rs.${item.price}<br>`
+    }
+    return str;
 }
