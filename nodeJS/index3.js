74da57174ddd11fdecc7dd8923c0bbd412ed8e39
Manojkumar357 <akulamanojkumar357@gmail.com>
2025-07-04 11:33:28 +0530

    update

--- nodeJS/index3.js
+++ nodeJS/index3.js
@@ -15,15 +15,36 @@ const users=[
 },
 
 ]
-
+//middlewares
 app.use(express.json());
+const auth = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Token missing or invalid" });
+  }
+
+  token = token.split(" ")[1];
+
+  try {
+    const user = jwt.verify(token, SECRET);
+    req.role = user.role; // attach role to request
+    next(); // move to the route
+  } catch (err) {
+    return res.status(403).json({ message: "Token verification failed" });
+  }
+};
+
+
+
+
 
 app.post("/login",(req,res)=>{
   const {email,pass}=req.body;
   const found=users.find((user)=>user.email===email && user.pass===pass);
   if(found){
     const token=jwt.sign(found,SECRET,{expiresIn:"1h"} );
-    res.status(200).json({user:found},{token});
+    res.status(200).json({user:found,token});
   }else{
     res.status(400).json({message:"Acess Denied"});
   }
@@ -34,3 +55,8 @@ app.post("/login",(req,res)=>{
 app.listen(3000,()=>{
     console.log("server started at 3000");
 })
+
+
+app.get("/users",auth,(req,res)=>{
+  res.json(users);
+});
